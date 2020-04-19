0a1d349123b43da4e4f5fb03bf51edd648fd192c
Adnan Kamili <adnan.kamili@cryptlex.com>
2020-04-19 13:23:54 +0000

    updated string functions

--- cstring.go
+++ cstring.go
@@ -4,17 +4,22 @@ package lexactivator
 
 import "C"
 
-const(
-	MaxCArrayLength C.uint = 256
-	MaxGoArrayLength C.int = 256
+const (
+	MaxCArrayLength  C.uint = 256
+	MaxGoArrayLength C.int  = 256
 )
 
-func ToCString(data string) *C.char {
+func GoToCString(data string) *C.char {
 	cString := C.CString(data)
 	return cString
 }
 
+func CtoGoString(cString *C.char) string {
+	goString := C.GoStringN(cString, MaxGoArrayLength)
+	return goString
+}
+
 func GetCArray() [MaxCArrayLength]C.char {
 	var cArray [MaxCArrayLength]C.char
-	return cArray;
+	return cArray
 }
--- wcstring.go
+++ wcstring.go
@@ -6,21 +6,39 @@ import "C"
 import (
 	"unicode/utf16"
 	"unsafe"
+	"bytes"
+	"encoding/binary"
 )
 
 const (
 	MaxCArrayLength  C.uint = 256
-	MaxGoArrayLength C.int    = 256
+	MaxGoArrayLength C.int  = 256
 )
 
-func ToCString(data string) *C.ushort {
-	runeByte := []rune(data)
+func GoToCString(goString string) *C.ushort {
+	runeByte := []rune(goString)
 	encodedByte := utf16.Encode(runeByte)
 	cString := (*C.ushort)(unsafe.Pointer(&encodedByte[0]))
 	return cString
 }
 
+func CtoGoString(cString *C.ushort) string {
+	encodedBytes := C.GoBytes(unsafe.Pointer(cString), MaxGoArrayLength)
+	goString,_ := decodeUtf16(encodedBytes, binary.LittleEndian)
+	return goString
+	//decodedBytes := utf16.Decode(encodedBytes)
+	//return string(decodedBytes)
+}
+
 func GetCArray() [MaxCArrayLength]C.ushort {
-	var cArray [MaxCArrayLength]C.char
-	return cArray;
+	var cArray [MaxCArrayLength]C.ushort
+	return cArray
+}
+
+func decodeUtf16(b []byte, order binary.ByteOrder) (string, error) {
+	ints := make([]uint16, len(b)/2)
+	if err := binary.Read(bytes.NewReader(b), order, &ints); err != nil {
+		return "", err
+	}
+	return string(utf16.Decode(ints)), nil
 }
