21def8ef6c87dbb5d4072ec37729fd75f7694370
Ahmad-Kemsan <ahmadkemsan@gmail.com>
2020-11-25 18:00:22 +0530

    updated libs

--- download_libs/download_libs.go
+++ download_libs/download_libs.go
@@ -13,7 +13,7 @@ import (
 
 func main() {
 	baseURL := "https://dl.cryptlex.com/downloads/"
-	libVersion := "v3.12.1"
+	libVersion := "v3.12.2"
 	basePath := "./libs/"
 	fmt.Println("Downloading LexActivator libs " + libVersion + " ...")
 	url := baseURL + libVersion + "/LexActivator-Static-Mac.zip"
