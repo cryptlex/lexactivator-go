74fefed51e796c8c1a33542ef001247a867125b2
Ahmad Kemsan <ahmadkemsan@gmail.com>
2023-07-28 16:50:08 +0530

    refactor: updated release callback in sample

--- examples/sample.go
+++ examples/sample.go
@@ -20,6 +20,20 @@ func licenseCallback(status int) {
 	}
 }
 
+func softwareReleaseUpdateCallback(status int, release *lexactivator.Release, userData interface{} ) {
+	if status == lexactivator.LA_RELEASE_UPDATE_AVAILABLE {
+		fmt.Println("A new update is available for the app!")
+		fmt.Println("Release notes: ", release.Notes)
+	} else if status == lexactivator.LA_RELEASE_UPDATE_AVAILABLE_NOT_ALLOWED {
+		fmt.Println("A new update is available for the app but it's not allowed!")
+		fmt.Println("Release notes: ", release.Notes)
+	} else if status == lexactivator.LA_RELEASE_UPDATE_NOT_AVAILABLE {
+		fmt.Println("Current version is already latest!")
+	} else {
+		fmt.Println("Error code: ", status)
+	}
+}
+
 func initData() {
 	var status int
 	status = lexactivator.SetProductData("PASTE_CONTENT_OF_PRODUCT.DAT_FILE")
@@ -39,6 +53,19 @@ func initData() {
 		fmt.Println("Error Code:", status)
 		os.Exit(1)
 	}
+	// set the actual channel of the release e.g stable
+	status = lexactivator.SetReleaseChannel("RELEASE_CHANNEL")
+	if lexactivator.LA_OK != status {
+		fmt.Println("Error Code:", status)
+		os.Exit(1)
+	}
+	// set the actual platform of the release e.g windows, macos, linux
+	status = lexactivator.SetReleasePlatform("RELEASE_PLATFORM")
+	if lexactivator.LA_OK != status {
+		fmt.Println("Error Code:", status)
+		os.Exit(1)
+	}
+
 }
 
 func activate() {
@@ -118,6 +145,13 @@ func main() {
 			activateTrial()
 		}
 	}
+	// Checking for software release update
+	// status = lexactivator.CheckReleaseUpdate(softwareReleaseUpdateCallback, lexactivator.LA_RELEASES_ALL, nil);
+	// if lexactivator.LA_OK != status {
+	// 	fmt.Println("Error Code:", status)
+	// 	os.Exit(1)
+	// }
+
 	fmt.Println("Press any key to exit...")
 	bufio.NewReader(os.Stdin).ReadByte()
 }
