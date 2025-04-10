f536433f31a581af129a132df7a09481db73a972
muneebkq <muneebkq05@gmail.com>
2025-04-10 15:07:15 +0530

    chore: add new status codes

--- lexstatus_codes.go
+++ lexstatus_codes.go
@@ -205,6 +205,9 @@ const (
 	// Client error.
 	LA_E_CLIENT int = 92
 
+	// Invalid account ID.
+	LA_E_ACCOUNT_ID = 93
+
 	// The user account has been temporarily locked for 5 mins due to 5 failed attempts.
 	LA_E_LOGIN_TEMPORARILY_LOCKED int = 100
 
@@ -225,4 +228,13 @@ const (
 
 	// The free plan has reached its activation limit.
 	LA_E_FREE_PLAN_ACTIVATION_LIMIT_REACHED int = 106
+
+	// The feature entitlements are invalid.
+    LA_E_FEATURE_ENTITLEMENTS_INVALID = 107
+
+    // The feature entitlement does not exist.
+    LA_E_FEATURE_ENTITLEMENT_NOT_FOUND = 108
+
+    // The entitlement set is not linked
+    LA_E_ENTITLEMENT_SET_NOT_LINKED = 109
 )
