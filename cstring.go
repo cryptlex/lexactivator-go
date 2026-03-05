d347fa9da877d9009183e5d40a9ef5be0251f481
Huzaif Mushtaq Mir <huzaif@Huzaif-MacBook-Air.local>
2026-03-05 15:46:52 +0530

    feat: increase license metadata buffer size

--- cstring.go
+++ cstring.go
@@ -6,7 +6,8 @@ package lexactivator
 import "C"
 import "unsafe"
 
-const maxCArrayLength  C.uint = 1024
+const cArrayLength C.uint = 1024
+const maxCArrayLength C.uint = 4096
 
 func goToCString(data string) *C.char {
 	cString := C.CString(data)
@@ -18,9 +19,12 @@ func ctoGoString(cString *C.char) string {
 	return goString
 }
 
-func getCArray() [maxCArrayLength]C.char {
-	var cArray [maxCArrayLength]C.char
-	return cArray
+func getCArray(length ...C.uint) []C.char {
+	size := cArrayLength
+	if len(length) > 0 {
+		size = length[0]
+	}
+	return make([]C.char, size)
 }
 
 func freeCString(cString *C.char) {
